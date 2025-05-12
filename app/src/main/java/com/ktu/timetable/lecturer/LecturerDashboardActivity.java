8e6f17f7e79d8c124a8ae56af268e1e6a0b50833
12liyab <ktliyab7@gmail.com>
2025-05-13 05:42:15 +0800

    Update LecturerDashboardActivity.java

--- app/src/main/java/com/ktu/timetable/lecturer/LecturerDashboardActivity.java
+++ app/src/main/java/com/ktu/timetable/lecturer/LecturerDashboardActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.TextView; 
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
