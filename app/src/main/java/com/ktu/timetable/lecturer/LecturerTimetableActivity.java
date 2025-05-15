924cb74fcd0fdee96166438747718e483fcf7249
12liyab <ktliyab7@gmail.com>
2025-05-16 07:29:33 +0800

    Update LecturerTimetableActivity.java

--- app/src/main/java/com/ktu/timetable/lecturer/LecturerTimetableActivity.java
+++ app/src/main/java/com/ktu/timetable/lecturer/LecturerTimetableActivity.java
@@ -2,7 +2,7 @@ package com.ktu.timetable.lecturer;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View; 
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
